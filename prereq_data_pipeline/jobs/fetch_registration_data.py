d504d7df5b4c6684d0c21477b79e9e14d8f40436
Stephen De Vight <devights@uw.edu>
2021-06-30 13:27:08 -0700

    fetch reg tests

--- prereq_data_pipeline/jobs/fetch_registration_data.py
+++ prereq_data_pipeline/jobs/fetch_registration_data.py
@@ -10,40 +10,37 @@ def run():
     session = db.get_session()
 
     _delete_registrations(session)
-    _get_registrations(session)
+    registrations = _get_registrations()
+    _save_registrations(session, registrations)
 
 
 # get registration data
-def _get_registrations(session):
+def _get_registrations():
     registrations = get_registrations_since_year(REGISTRATION_START_YEAR)
-    chunk_size = 10000
-    chunks = [registrations[x:x+chunk_size] for x in
-              range(0, len(registrations), chunk_size)]
 
-    # process and save a batch of registrations
-    def process_registrations(registrations):
-        registration_objects = []
-        for index, registration in registrations.iterrows():
-            registration_objects.append(
-                Registration(
-                    system_key=registration['system_key'],
-                    regis_yr=registration['regis_yr'],
-                    regis_qtr=registration['regis_qtr'],
-                    crs_curric_abbr=registration['crs_curric_abbr'].strip(),
-                    crs_number=registration['crs_number']
-                )
+    registration_objects = []
+    for index, registration in registrations.iterrows():
+        registration_objects.append(
+            Registration(
+                system_key=registration['system_key'],
+                regis_yr=registration['regis_yr'],
+                regis_qtr=registration['regis_qtr'],
+                crs_curric_abbr=registration['crs_curric_abbr'].strip(),
+                crs_number=registration['crs_number']
             )
-        return registration_objects
-
-    for chunk in chunks:
-        objs = process_registrations(chunk)
-        _save_registrations(session, objs)
+        )
+    return registration_objects
 
 
 # save registration data
 def _save_registrations(session, registrations):
-    session.add_all(registrations)
-    session.commit()
+    chunk_size = 10000
+    chunks = [registrations[x:x + chunk_size] for x in
+              range(0, len(registrations), chunk_size)]
+
+    for chunk in chunks:
+        session.add_all(chunk)
+        session.commit()
 
 
 # delete existing registration data
