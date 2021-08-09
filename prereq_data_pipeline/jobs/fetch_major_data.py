610b1f90f78996778c88617958c62beb4ef581bb
Stephen De Vight <devights@uw.edu>
2021-08-09 13:17:47 -0700

    fetch major

--- prereq_data_pipeline/jobs/fetch_major_data.py
+++ prereq_data_pipeline/jobs/fetch_major_data.py
@@ -1,48 +1,41 @@
 from prereq_data_pipeline.dao.edw import get_majors
 from prereq_data_pipeline.models.major import Major
-from prereq_data_pipeline.databases.implementation import get_db_implemenation
+from prereq_data_pipeline.jobs import DataJob
 
 
-def run():
-    db = get_db_implemenation()
-    session = db.get_session()
+class FetchMajorData(DataJob):
+    def run(self):
+        self._delete_majors()
+        majors = self._get_majors()
+        self._save_majors(majors)
 
-    _delete_majors(session)
-    majors = _get_majors()
-    _save_majors(session, majors)
+    # get major data
+    def _get_majors(self):
+        majors = get_majors()
 
+        major_objects = []
+        for index, major in majors.iterrows():
+            major_obj = Major(
+                program_code=major['program_code'],
+                program_title=major['program_title'],
+                program_department=major['program_department'],
+                program_description=major['program_description'],
+                program_level=major['program_level'],
+                program_type=major['program_type'],
+                program_school_or_college=major['program_school_or_college'],
+                program_dateStartLabel=major['program_dateStartLabel'],
+                program_dateEndLabel=major['program_dateEndLabel'],
+                campus_name=major['campus_name'],
+                program_admissionType=major['program_admissionType']
+            )
+            major_objects.append(major_obj)
+        return major_objects
 
-# get major data
-def _get_majors():
-    majors = get_majors()
+    # save major data
+    def _save_majors(self, majors):
+        self.session.add_all(majors)
+        self.session.commit()
 
-    major_objects = []
-    for index, major in majors.iterrows():
-        major_obj = Major(
-            program_code=major['program_code'],
-            program_title=major['program_title'],
-            program_department=major['program_department'],
-            program_description=major['program_description'],
-            program_level=major['program_level'],
-            program_type=major['program_type'],
-            program_school_or_college=major['program_school_or_college'],
-            program_dateStartLabel=major['program_dateStartLabel'],
-            program_dateEndLabel=major['program_dateEndLabel'],
-            campus_name=major['campus_name'],
-            program_admissionType=major['program_admissionType']
-        )
-        major_objects.append(major_obj)
-    return major_objects
-
-
-# save major data
-def _save_majors(session, majors):
-    session.add_all(majors)
-    session.commit()
-
-
-# delete existing major data
-def _delete_majors(session):
-    q = session.query(Major)
-    q.delete()
-    session.commit()
+    # delete existing major data
+    def _delete_majors(self):
+        self._delete_objects(Major)
